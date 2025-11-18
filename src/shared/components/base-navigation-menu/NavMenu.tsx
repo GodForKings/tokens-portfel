5856dab4efa0145b294be63df10ad01245123061
GodForKings <white_king_of_gangster@mail.ru>
2025-11-19 02:25:32 +0700

    fix(bug navmenu): #7

--- src/shared/components/base-navigation-menu/NavMenu.tsx
+++ src/shared/components/base-navigation-menu/NavMenu.tsx
@@ -1,3 +1,4 @@
+'use client'
 import type { FC } from 'react'
 
 import Link from 'next/link'
@@ -10,50 +11,11 @@ import {
 	NavigationMenuPopup,
 	NavigationMenuPositioner,
 	NavigationMenuTrigger,
-	navigationMenuTriggerStyle,
 } from '@/shared/components/ui/base-navigation-menu'
 import { CircleCheckIcon, CircleHelpIcon, CircleIcon } from 'lucide-react'
 
 import { Data, PAGES, cn } from '@/shared'
 import { CATEGORY_TOKENS } from '@/shared/constants'
-
-const components: { title: string; href: string; description: string }[] = [
-	{
-		title: 'Alert Dialog',
-		href: '/docs/primitives/alert-dialog',
-		description:
-			'A modal dialog that interrupts the user with important content and expects a response.',
-	},
-	{
-		title: 'Hover Card',
-		href: '/docs/primitives/hover-card',
-		description:
-			'For sighted users to preview content available behind a link.',
-	},
-	{
-		title: 'Progress',
-		href: '/docs/primitives/progress',
-		description:
-			'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
-	},
-	{
-		title: 'Scroll-area',
-		href: '/docs/primitives/scroll-area',
-		description: 'Visually or semantically separates content.',
-	},
-	{
-		title: 'Tabs',
-		href: '/docs/primitives/tabs',
-		description:
-			'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
-	},
-	{
-		title: 'Tooltip',
-		href: '/docs/primitives/tooltip',
-		description:
-			'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
-	},
-]
 
 function ListItem({
 	title,
@@ -134,11 +96,9 @@ const NavMenu: FC<NavMenuProps> = props => {
 							)}
 						>
 							{Object.entries(CATEGORY_TOKENS).map(([label, slug]) => (
-								<ListItem
-									key={slug}
-									href={PAGES.CATEGORY(slug)}
-									title={label}
-								></ListItem>
+								<ListItem key={slug} href={PAGES.CATEGORY(slug)} title={label}>
+									...
+								</ListItem>
 							))}
 						</ul>
 					</NavigationMenuContent>
